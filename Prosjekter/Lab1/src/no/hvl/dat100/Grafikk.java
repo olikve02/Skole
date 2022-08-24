f7f01d723e3244e896ed5235fc14bd5e5964e3c8
Oliver Kvebek <oliver.kvebek@hotmail.com>
2022-08-24 23:42:26 +0200

    changes

--- Prosjekter/Lab1/src/no/hvl/dat100/Grafikk.java
+++ Prosjekter/Lab1/src/no/hvl/dat100/Grafikk.java
@@ -8,8 +8,15 @@ public class Grafikk extends EasyGraphics {
         launch(args);
     }
     public void run(){
-        makeWindow("Grafikk", 350, 150);
+        makeWindow("Grafikk", 1000, 1000);
         drawCircle(150, 70, 60);
+        setColor(255,200,0);
+        fillCircle(150,70,60);
+
+        drawRectangle(500, 400,70,40);
+        fillRectangle(500, 400,70,40);
+
+
     }
 
 }
